0902efeb49aba2f9f3d24bb7c2108d1ff5036151
robsantox <99149832+robsantox@users.noreply.github.com>
2022-03-10 21:11:40 -0300

    Add files via upload

---
+++ PICPRO - Kenzie e PicPay/assets/js/script.js
@@ -0,0 +1,27 @@
+const listaOpcoes = document.querySelector(
+  ".secaoDetalhesConta__opcoesTransacoes"
+);
+
+listaOpcoes.addEventListener("click", indentificarOpcoes);
+
+function indentificarOpcoes(event) {
+  const elemento = event.target;
+
+  if (elemento.tagName == "LI") {
+    const id = elemento.id;
+
+    const secaoEscolha = document.querySelector(`div[data-id="${id}"]`);
+
+    removeClasseMostrar();
+
+    secaoEscolha.classList.add("mostrar");
+  }
+}
+
+function removeClasseMostrar() {
+  const divs = document.querySelectorAll(".secaoTransacao .container div");
+
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].classList.remove("mostrar");
+  }
+}
